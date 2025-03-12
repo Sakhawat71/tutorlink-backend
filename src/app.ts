b16dcc22ce0fa28b743f581cf4deac46caa94655
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-03-12 21:43:34 +0600

    Basic setup for server

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+const port = 3000;
+
+
+const app: Application = express();
+
+
+const corsOptions = {
+    origin: ["http://localhost:3000"],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        status: 200,
+        message: "TutorLink server is running..... .... ... .. ."
+    })
+})
+
+
+export default app;
