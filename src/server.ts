92f82fc32d70e611c5d022d2f0eb25e9539c9aa9
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-03-12 21:57:58 +0600

    Add MongoDB connection and ESLint configuration

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
 import app from "./app"
 import config from './app/config';
+import mongoose from 'mongoose';
+
+
+
+
+// main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
 app.listen(config.port, () => {
     console.log(`Example app listening on port ${config.port}`)
