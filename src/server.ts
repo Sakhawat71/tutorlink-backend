f57484ef1b6c760312e931c5ea8f0001670cc1b5
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-03-12 22:14:35 +0600

    Add ESLint configuration, update server to use environment variables, and create .env.example

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
+import { Server } from "http";
 import app from "./app"
 import config from './app/config';
 import mongoose from 'mongoose';
 
-
-
-
-// main().catch(err => console.log(err));
+let server : Server;
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
-
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    try {
+        await mongoose.connect(config.db_url as string);
+        server = app.listen(config.port, () => {
+            console.log(`TutorLink app listening on port ${config.port}`)
+        });
+    } catch (error) {
+        console.error("Error occurred during startup:", error);
+        process.exit(1);
+    }
 }
-
-app.listen(config.port, () => {
-    console.log(`Example app listening on port ${config.port}`)
-})
+main();
