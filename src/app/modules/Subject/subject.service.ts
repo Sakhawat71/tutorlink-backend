5ae01f1a083b0cb2d492012bc438f61d46f3158e
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-04-01 03:52:43 +0600

    Enhance subject retrieval by excluding password from tutor data and applying lean query for performance

--- src/app/modules/Subject/subject.service.ts
+++ src/app/modules/Subject/subject.service.ts
@@ -3,17 +3,25 @@ import { TutorSubjectModel } from "./subject.model";
 
 const getSubjectsFormDB = async () => {
     return await TutorSubjectModel
-    .find()
-    .populate("tutor");
+        .find()
+        .populate({
+            path: "tutor",
+            select : "-password",
+        })
+        .lean();
 };
 
 const getSubjectsByIdFormDB = async (id: string) => {
     return await TutorSubjectModel
-    .findById(id)
-    .populate("tutor");
+        .findById(id)
+        .populate({
+            path: "tutor",
+            select: "-password",
+        });
+
 };
 
-const createSubjectInDB = async (data : ITutorSubject) => {
+const createSubjectInDB = async (data: ITutorSubject) => {
     return await TutorSubjectModel.create(data);
 };
 
